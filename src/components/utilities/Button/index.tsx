85b900d923db8ebb0d4e73a71e8510b13b5b986b
lilyx13 <ashlyn@lilyx.io>
2023-06-27 12:59:46 -0600

    button component props configured
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/utilities/Button/index.tsx
+++ src/components/utilities/Button/index.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 
 // TODO: Set up props to make the button reusable
-
-export default function Button() {
+interface ButtonProps {
+  readonly method?: () => void;
+  readonly text: string;
+}
+export default function Button({ text, method }: ButtonProps) {
   const [count, setCount] = useState(0);
   return (
     <button
-      onClick={() => setCount((count) => count + 1)}
-      className="bg-blue-500 px-6 py-4 rounded-md text-white hover:bg-blue-800 hover:shadow-md transition ease-linear duration-150"
+      onClick={method}
+      className="bg-blue-500 text-white hover:bg-blue-800 rounded-md px-6 py-4 transition duration-150 ease-linear hover:shadow-md"
     >
-      count is {count}
+      {text}
     </button>
   );
 }
